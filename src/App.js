52c9948d0d93a4523eb34c07909982546ee51523
Sarah Sajjad <sarahsajjad93@gmail.com>
2023-04-05 13:50:25 +0500

    React router added for routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WalletDashboard from './App/pages/WalletDashboard';
 import Login from './App/pages/Login';
 
 function App() {
   return (
-    <>
-      <WalletDashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<WalletDashboard />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 export default App;
